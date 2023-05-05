5c3a89d9de9a3d6ba2b00928c4d374aaba137db7
Knight-4000 <yorksprinters@gmail.com>
2023-05-05 15:06:13 -0400

    Dryer still.

--- script.js
+++ script.js
@@ -15,38 +15,24 @@ function imageMode(color) {
     // using ` instead of ' converts above to template string
 }
 
-
-// Dark Mode
-
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode'
-   // toggleIcon.children[1].classList.remove('fa-sun');
-   // toggleIcon.children[1].classList.add('fa-moon');
-   // Replace fa-sun with fa-moon
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
-}
-
-// Light Mode
-
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'; 
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode'
+function toggleDarkLightMode(isDark) {
+    // isDark is a ternary operator
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ?'Dark Mode' :'Light Mode';
+    isDark? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
+    isDark ? imageMode('dark') : imageMode('light');
 }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+        toggleDarkLightMode(true);
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        toggleDarkLightMode(false);
     }
 }
 // Event Listener
